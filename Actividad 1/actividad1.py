0d41825a1a2dceea1140a2b77ff1e388b2073b9d
andres soto <sotoluque2000@gmail.com>
2025-05-02 14:14:43 -0500

    Se inicia las primeras lineas viendo como funciona los type

---
+++ Actividad 1/actividad1.py
@@ -0,0 +1,18 @@
+caja1 = 45
+caja2 = 3.1416
+caja3 = "Hola soy andres"
+caja4 = False
+caja5 = [1, 2, 3, 4]
+caja6 = ("luna", "sol")
+caja7 = {"nombre": "Andres", "edad" : 18}
+caja8 = {"manzana", "pera", "manzana"}
+
+
+print("caja 1:", type(caja1))
+print("caja 2:", type(caja2))
+print("caja 3:", type(caja3))
+print("caja 4:", type(caja4))
+print("caja 5:", type(caja5))
+print("caja 6:", type(caja6))
+print("caja 7:", type(caja7))
+print("caja 8:", type(caja8))
